9b3d4bba3a79b3dbadb6015db22509d3a2e9e50c
rnnbalonga <rnnbalongapro@gmail.com>
2025-10-25 09:41:27 +0900

    Finish challenge

--- days/day-2-word-frequency/main.py
+++ days/day-2-word-frequency/main.py
@@ -5,4 +5,13 @@
 import re
 
 with open("log.txt", "r") as log_file:
-    pass
+    lines = log_file.readlines(1)
+    pattern = r"\[(\w{3})\s\w{3}\s\d{2}\s(\d{2}):\d{2}:\d{2}\s\d{4}\]\s\[(.*?)\]\s(.*)"
+    for line in lines:
+        match = re.match(pattern, line)
+        if pattern:
+            day_of_week, hour, level, message = match.groups()
+            print(f"Day of week: {day_of_week}")
+            print(f"Hour: {hour}")
+            print(f"Level: {level}")
+            print(f"Message: {message}")
