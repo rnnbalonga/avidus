5e7f6bba02d8a9c8fc34a407ea5250374ff0a298
rnnbalonga <rnnbalongapro@gmail.com>
2025-10-25 11:23:37 +0900

    Add reverse_second_word function

--- days/day-3-reverse-strings/main.py
+++ days/day-3-reverse-strings/main.py
@@ -3,14 +3,20 @@
 # Create a cleaner looking file.
 # Arrange lines neatly. One line (poem) per line in new file.
 # One line break in between stanzas.
+
+
 def arrange_lines(text):
     line = text.split(".")
     for sentence in line:
         if sentence != "":
             print(f"{sentence.strip()}.")
 
+def reverse_second_word(sentence):
+    second_word = sentence.split()[1]
+    return second_word[::-1]
+
 with open("poem.txt", "r") as file:
     content = file.readlines()
     for line in content:
         line = line.strip()
-        arrange_lines(line)
+        line = arrange_lines(line)
