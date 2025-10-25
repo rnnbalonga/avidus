fd1b2d98227c6d7872a8032fba377761d6ab4671
rnnbalonga <rnnbalongapro@gmail.com>
2025-10-25 11:04:53 +0900

    Add arrange_lines function and make it work.

--- days/day-3-reverse-strings/main.py
+++ days/day-3-reverse-strings/main.py
@@ -1,1 +1,16 @@
 #Mission: Reverse every second word of a string.
+#Bonus:
+# Create a cleaner looking file.
+# Arrange lines neatly. One line (poem) per line in new file.
+# One line break in between stanzas.
+def arrange_lines(text):
+    line = text.split(".")
+    for sentence in line:
+        if sentence != "":
+            print(f"{sentence.strip()}.")
+
+with open("poem.txt", "r") as file:
+    content = file.readlines()
+    for line in content:
+        line = line.strip()
+        arrange_lines(line)
