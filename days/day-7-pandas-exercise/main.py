05f9e80a49fd7dc8bcf7af5dcdd8504b5b331f3c
rnnbalonga <rnnbalongapro@gmail.com>
2025-11-05 22:53:53 +0900

    edit values in leads2.csv

--- days/day-7-pandas-exercise/main.py
+++ days/day-7-pandas-exercise/main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-df_1 = pd.read_csv("leads1.csv")
-df_2 = pd.read_csv("leads2.csv")
+df_1 = pd.read_csv("leads1.csv", sep="|")
+df_2 = pd.read_csv("leads2.csv", sep="|")
+
+common = pd.merge(df_1, df_2, on='name', how='outer', indicator=True)
+
+hey = pd.concat([df_1,df_2]).drop_duplicates(keep=False, subset='name')
 
 def main():
-    print(df_1)
+    print(hey)
 
 if __name__ == "__main__":
     main()
