90c03bf6ca52e696a411720b400e1ec89d31ccb1
rnnbalonga <rnnbalongapro@gmail.com>
2026-04-28 21:19:44 +0900

    Fix some things

--- days/day-7-pandas-exercise/main.py
+++ days/day-7-pandas-exercise/main.py
@@ -7,8 +7,16 @@ common = pd.merge(df_1, df_2, on='name', how='outer', indicator=True)
 
 hey = pd.concat([df_1,df_2]).drop_duplicates(keep=False, subset='name')
 
+# Find rows that are only in df1 but not in df2
+diff_df1 = common[common['_merge'] == 'left_only']
+
+# Find rows that are only in df2 but not in df1
+diff_df2 = common[common['_merge'] == 'right_only']
+
 def main():
-    print(hey)
+    print("leads1 only customers")
+    for name in diff_df1['name']:
+        print(name)
 
 if __name__ == "__main__":
     main()
